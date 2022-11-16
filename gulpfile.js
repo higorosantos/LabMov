38bbbed819da7d6c4837e85a96040abb305ea211
Higor Oliveira dos Santos <higorosantos@gmail.com>
2022-11-16 17:05:55 -0300

    adcioando o gulp e o bootstrap ao projeto. Refatorando parte do CSS

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const { series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const watch = require('gulp-watch');
+
+function SassToCss(){
+
+    return  gulp.src(['./src/assets/scss/*.scss','./node_modules/bootstrap/scss/*.scss'])
+        .pipe(sass({ outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./src/assets/css'))
+
+}
+
+function Watcher(){
+
+    return watch(['./src/assets/scss/**/*.scss'], ()=> {
+        SassToCss();
+    })
+
+}
+
+
+module.exports.default = parallel(SassToCss,Watcher);
